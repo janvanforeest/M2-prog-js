694d320ca5023e7ddee02f6356a6f46d82f10e5d
jan van foreest <30220@ma-web.nl>
2022-11-21 10:04:55 +0100

    proggrameerjavales1klaar

---
+++ les1/app.js
@@ -0,0 +1,24 @@
+class app
+{
+    runapplication()
+    {
+        console.log("hallo wereld")
+        let appnaam ="jgjfnf";
+        let versienaam = 2.4;
+        let versiedatum = Date;
+        let authur = "jandecoolen";
+        let copyright = "janusinc";
+        let distributeur = "jandedistribeurder";
+        let darkmode = true;
+        console.log(appnaam)
+        console.log(versienaam)
+        console.log(versiedatum)
+        console.log(authur)
+        console.log(copyright)
+        console.log(distributeur)
+        console.log(darkmode)
+
+    }
+}
+let app = new app();
+app.runapplication();
